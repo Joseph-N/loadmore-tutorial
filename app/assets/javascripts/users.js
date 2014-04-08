f29a3f7978737dee19c5e6b0e0660cd188bb1900
Joseph-N <josendungu@hotmail.com>
2014-04-08 19:08:44 +0300

    added loading gif when making ajax call

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -6,11 +6,18 @@ $(document).ready(function(){
 		// prevent the default click action
 		e.preventDefault();
 
+        // hide load more link
+        $('.load-more').hide();
+
+        // show loading gif
+        $('.loading-gif').show();
+
 		// get the last id and save it in a variable 'last-id'
         var last_id = $('.record').last().attr('data-id');
 
         // make an ajax call passing along our last user id
         $.ajax({
+
         	// make a get request to the server
             type: "GET",
             // get the url from the href attribute of our link
@@ -18,7 +25,15 @@ $(document).ready(function(){
             // send the last id to our rails app
             data: { id: last_id },
             // the response will be a script
-            dataType: "script"
+            dataType: "script",
+
+            // upon success 
+            success: function(){
+                // hide the loading gif
+                $('.loading-gif').hide();
+                // show our load more link
+                $('.load-more').show();
+            }
         });
 
 	});
